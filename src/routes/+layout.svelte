f9468da16e52314c0c04973b270a4b52e9c24393
RenatoLomba <rntlomba@gmail.com>
2023-06-03 21:58:37 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav class="flex gap-4">
+	<a href="/">Home</a>
+	<a href="/posts">Posts</a>
+</nav>
+
+<div class="mt-4">
+	<slot />
+</div>
+
+<style lang="postcss">
+	nav a {
+		@apply text-blue-500;
+	}
+</style>

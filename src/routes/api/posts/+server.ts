4aec6ba258f95736be19ef372313bd07d202423e
RenatoLomba <rntlomba@gmail.com>
2023-06-04 16:42:40 -0300

    caching request with response

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -3,8 +3,14 @@ import type { RequestHandler } from './$types';
 import { prisma } from '$lib/server/database';
 import { json } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async () => {
-	const posts = await prisma.post.findMany();
+export const GET: RequestHandler = async (event) => {
+	const posts = await prisma.post.findMany({
+		take: Math.round(Math.random() * 30),
+	});
+
+	event.setHeaders({
+		'Cache-Control': 'max-age=60',
+	});
 
 	return json(posts, { status: 200 });
 };

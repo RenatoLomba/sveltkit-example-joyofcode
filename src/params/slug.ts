298918c2feffe2b276b2e19ac306e14823ce2e3a
RenatoLomba <rntlomba@gmail.com>
2023-06-03 23:35:10 -0300

    matching params

---
+++ src/params/slug.ts
@@ -0,0 +1,7 @@
+import type { ParamMatcher } from '@sveltejs/kit';
+
+export const match: ParamMatcher = (param) => {
+	const slugRegex = /^[a-z0-9]+(?:-[a-z0-9]+)*$/;
+
+	return slugRegex.test(param);
+};

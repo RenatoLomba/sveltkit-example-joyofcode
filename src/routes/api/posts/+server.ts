ec5eaa360c7e47b8082abc1cfe0113e7b5f4b7be
RenatoLomba <rntlomba@gmail.com>
2023-06-04 17:00:07 -0300

    client side data fetching

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -8,9 +8,15 @@ export const GET: RequestHandler = async (event) => {
 		take: Math.round(Math.random() * 30),
 	});
 
+	// Cache return same response for 60 seconds (on disk)
 	event.setHeaders({
 		'Cache-Control': 'max-age=60',
 	});
+
+	// Cache return same response for every request on web for 60 seconds (on server)
+	// event.setHeaders({
+	// 	'Cache-Control': 'public, max-age=0, s-maxage=60',
+	// });
 
 	return json(posts, { status: 200 });
 };
--- src/routes/posts/+layout.svelte
+++ src/routes/posts/+layout.svelte
@@ -1,12 +1,33 @@
+<script lang="ts">
+	import type { Post } from '@prisma/client';
+
+	async function getPosts() {
+		const response = await fetch('/api/posts');
+		const posts: Post[] = await response.json();
+		return posts;
+	}
+</script>
+
 <div class="w-full flex mt-14">
 	<aside class="max-w-[200px] w-full">
 		<h4 class="text-2xl font-medium">Posts</h4>
 
 		<nav>
 			<ul class="mt-4">
-				<li><a href="/posts/post-1">Post 1</a></li>
-				<li><a href="/posts/post-2">Post 2</a></li>
-				<li><a href="/posts/post-3">Post 3</a></li>
+				{#await getPosts()}
+					<p>Loading...</p>
+				{:then posts}
+					<p>Showing {posts.length} posts.</p>
+					{#each posts as post}
+						<li class="mt-2">
+							<a href={`/posts/${post.slug}`}>{post.title}</a>
+						</li>
+					{:else}
+						<p>No post found.</p>
+					{/each}
+				{:catch error}
+					<p>Something went wrong: {error.message}.</p>
+				{/await}
 			</ul>
 		</nav>
 	</aside>

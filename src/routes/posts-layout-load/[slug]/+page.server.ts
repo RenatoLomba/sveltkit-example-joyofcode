785d665447ef54740a99a8e9bc84fc85aedbad6b
RenatoLomba <rntlomba@gmail.com>
2023-06-05 18:19:37 -0300

    using parent data on load

--- src/routes/posts-layout-load/[slug]/+page.server.ts
+++ src/routes/posts-layout-load/[slug]/+page.server.ts
@@ -3,7 +3,9 @@ import { prisma } from '$lib/server/database';
 
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, parent }) => {
+	console.log(await parent());
+
 	const { slug } = params;
 
 	const post = await prisma.post.findUnique({
